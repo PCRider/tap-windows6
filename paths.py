511e370a666b65a6481e013be58412e6d715d6c3
Ehsan AlemMohammadGhasemlou <e.ghasemloo@gmail.com>
2018-09-13 14:30:27 +0700

    Change for Python3 and VisualStudio2017 and WDK1803

--- paths.py
+++ paths.py
@@ -1,5 +1,5 @@
 
 # Windows 10 EWDK (can just be ISO mount point)
-EWDK = "C:\\WinWDK\\16299"
+EWDK = "C:\\Program Files (x86)\\Microsoft Visual Studio\\2017\\Community\\Common7\\"
 
 NSIS = "C:\\Program Files (x86)\\NSIS"
